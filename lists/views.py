c408b69f7098711fef72bd3c933128d2b9255e7b
Deep Sukhwani <deepsukhwani@gmail.com>
2019-09-08 11:17:02 +0530

    Tests now work with 1 list item

--- lists/views.py
+++ lists/views.py
@@ -4,10 +4,15 @@ from django.shortcuts import render
 
 # Create your views here.
 def home_page(request):
+
+    # if request.method == 'POST':
+    return render(request, 'home.html', {
+        'new_item_text': request.POST.get('item_text', ''),
+    })
+
     # Django's render function takes the request as its first param
     # and the name of the template to render
 
     # This will automatically search folders called "templates" inside any of
     # your apps' directories. Then it builds an HttpResponse for you, based on the
     # content of the template
-    return render(request, 'home.html')
