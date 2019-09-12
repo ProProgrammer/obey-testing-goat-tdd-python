bb900a3ec81fa9421dedd7c89e51c5c0e91e2307
Deep Sukhwani <deepsukhwani@gmail.com>
2019-09-12 15:29:14 +0530

    Add a url for creating new post and its related tests

--- lists/views.py
+++ lists/views.py
@@ -4,9 +4,6 @@ from lists.models import Item
 
 
 def home_page(request):
-    if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/lists/the-only-list-in-the-world/')
     return render(request, 'home.html')
 
     # Django's render function takes the request as its first param
@@ -27,3 +24,8 @@ def view_list(request):
     # your apps' directories. Then it builds an HttpResponse for you, based on the
     # content of the template
     return render(request, 'list.html', {'items': items})
+
+
+def new_list(request):
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect('/lists/the-only-list-in-the-world/')
