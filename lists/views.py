2f6b860b5306ccae51a534fbc0b44ce3775e8672
Deep Sukhwani <deepsukhwani@gmail.com>
2019-09-12 14:10:22 +0530

    new url, view and template to display the list, associated tests

--- lists/views.py
+++ lists/views.py
@@ -5,10 +5,19 @@ from lists.models import Item
 
 def home_page(request):
     if request.method == 'POST':
-        new_item_text = request.POST['item_text']
-        Item.objects.create(text=new_item_text)
-        return redirect('/')
+        Item.objects.create(text=request.POST['item_text'])
+        return redirect('/lists/the-only-list-in-the-world/')
+    return render(request, 'home.html')
 
+    # Django's render function takes the request as its first param
+    # and the name of the template to render
+
+    # This will automatically search folders called "templates" inside any of
+    # your apps' directories. Then it builds an HttpResponse for you, based on the
+    # content of the template
+
+
+def view_list(request):
     items = Item.objects.all()
 
     # Django's render function takes the request as its first param
@@ -17,4 +26,4 @@ def home_page(request):
     # This will automatically search folders called "templates" inside any of
     # your apps' directories. Then it builds an HttpResponse for you, based on the
     # content of the template
-    return render(request, 'home.html', {'items': items})
+    return render(request, 'list.html', {'items': items})
